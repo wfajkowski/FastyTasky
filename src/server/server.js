089b671ac9191720a0bccf95198f401ec99fae66
Rafał Kamka <rafalkamka@gmail.com>
2019-09-11 12:22:36 +0200

    webpack-dev-server added, port used by the server

--- src/server/server.js
+++ src/server/server.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
     res.sendFile(HTML_FILE)
 })
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`App listening to ${PORT}....`)
     console.log('Press Ctrl+C to quit.')
