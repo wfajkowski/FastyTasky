da15106badd7b5cec63ec0569ce286dd64f3da45
beataww <beawla@gmail.com>
2019-09-16 19:20:27 +0200

    sign in

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import './css/style.css';
 import './js/itemList.js';
 import './js/hamburger.js';
+import './js/loginBox.js';
 
 import {init} from './js/userLists';
+import {main_view} from './js/loginBox.js';
 
+const token = localStorage.getItem('x-auth-token');
+if(token){
+    main_view();
+    //tymczasowo usuwany po nowym wczytaniu - później przy wylogowaniu
+    localStorage.removeItem('x-auth-token');
+}
 init();
