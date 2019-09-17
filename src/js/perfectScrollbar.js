2276f7cc8b32a9b5ed8b98cb5dc35739f929711e
Emil Małańczak <kdddmalanczak@gmail.com>
2019-09-17 15:59:32 +0200

    css

--- src/js/perfectScrollbar.js
+++ src/js/perfectScrollbar.js
@@ -1,7 +1,7 @@
 import PerfectScrollbar from 'perfect-scrollbar';
 const ps = new PerfectScrollbar('.todo-lists', {
     wheelSpeed: .4,
-    maxScrollbarLength: 100,
-    minScrollbarLength: 20
+    maxScrollbarLength: 0,
+    
 });
 ps.update();
