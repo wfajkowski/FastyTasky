83fc38f02ad24f47957669c2b6a8b846a23eb9a1
wfajkowski <wfajkowski@gmail.com>
2019-09-18 23:23:49 +0200

    add logout functionality

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
-import './css/style.css';
-import './js/itemList.js';
-import './js/hamburger.js';
-import 'jquery';
-import './js/profilePic.js';
-import './js/loginBox.js';
-import './js/perfectScrollbar.js'
-import './js/tasks.js';
+import "./css/style.css";
+import "./js/itemList.js";
+import "./js/hamburger.js";
+import "jquery";
+import "./js/profilePic.js";
+import "./js/loginBox.js";
+import "./js/perfectScrollbar.js";
+import "./js/tasks.js";
 
-import {init} from './js/userLists';
-import {main_view} from './js/loginBox.js';
+import { init } from "./js/userLists";
+import { main_view } from "./js/loginBox.js";
 
-const token = localStorage.getItem('x-auth-token');
-if(token){
-    main_view();
-    //tymczasowo usuwany po nowym wczytaniu - później przy wylogowaniu
-    localStorage.removeItem('x-auth-token');
+const token = localStorage.getItem("x-auth-token");
+if (token) {
+  main_view();
 }
 init();
